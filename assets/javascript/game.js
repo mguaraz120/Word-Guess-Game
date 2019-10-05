0c38df256d0200a7ff965d0e3c01a7727071f085
Mario Vizcaino <vizhar1952@gmail.com>
2019-10-04 23:19:27 -0400

    added reset.css, added images and made adjustments in javascript and style.css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,45 +1,40 @@
 
 var targetDiv = document.getElementById("display");
 var userChoiceDiv = document.getElementById("user-choice");
-var consoleLogDiv = document.getElementById("console-log");
+var resultDiv = document.getElementById("result");
 var countryDiv = document.getElementById("country");
+
 var countries = ["Argentina", "Brazil", "Peru", "China", "USA", "Canada", "Vietnam", 
     "Japan", "Spain", "Russia", "Germany", "Colombia", "Mexico", "Ethiopia", "Egypt" ];
 var randomIndex = Math.floor(Math.random()*countries.length);
 var country = countries[randomIndex];
-countryDiv.textContent = country;
-//var length = country.length;
 var display = [country.length];
-// var win = length;
 var letters = country.split('');
 var attempts = letters.length + 2;
 var output = "";
-var storedLetters =[];
-// var userLetter= "";
 document.onkeyup = function(event)
 {   
     var userChoice = event.key;
     userChoiceDiv.textContent = userChoice;
-    storedLetters.push(userChoice);
-    //console.log(storedLetters);
     attempts--;
     for (var i=0; i<country.length; i++)
     {
         if (userChoice.toLowerCase()===letters[i].toLowerCase())
         {
-            consoleLogDiv.textContent = userChoice.toUpperCase();
+            resultDiv.textContent = userChoice.toUpperCase();
             updateDisplay(userChoice, i);
-        }         
+        } 
+       
     }
     console.log(display.find(ch => ch === "_"));
     if (display.find(ch => ch === "_") === undefined)  
     {
-        consoleLogDiv.textContent = "you win";
+        resultDiv.textContent = "you win";
     }
+
     if (attempts<1)
     {
-        consoleLogDiv.textContent = "you lose";
-
+        resultDiv.textContent = "you lose";
     }
    
 }
@@ -56,8 +51,4 @@ var setupBlanks = function()
         targetDiv.textContent = display.join(" ");    
     }
 }
-
-
 setupBlanks();
-// display = [""];
-// console.log(display.find(ch => ch === "_"));
